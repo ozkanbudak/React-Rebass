c6107017159702cd481b156ad9f71e9d3e6ff372
Ozkan Hasan BUDAK <ozkanhasanbudak@192.168.1.62>
2022-03-11 12:25:31 +0300

    master

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from 'emotion-theming';
+import Test from './Test';
+import theme from './theme'
+
+function App( ) {
+  return (
+      <Test>Test</Test>
+  );
+}
+
+export default App;
---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import { Text } from "rebass";
+
+function Test() {
+  return (
+    <Text fontSize={6} color="primary" fontWeight="bold">
+      Text
+    </Text>
+  );
+}
+
+export default Test;
