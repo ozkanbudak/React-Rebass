ee227a87fcdfad33c34264d941cbac0df5b4a397
Ozkan Hasan BUDAK <ozkanhasanbudak@192.168.1.62>
2022-03-11 21:46:16 +0300

    rebass

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
 import { ThemeProvider } from 'emotion-theming';
 import Test from './Test';
-import theme from './theme'
+import preset from '@rebass/preset'
+import _ from "lodash";
+import { Text } from "rebass";
+import { ConfigProvider, Button, Input } from 'antd';
+import { Typography } from 'antd';
+import { useState } from "react"
+import 'antd/dist/antd.variable.min.css';
+import './ant.less';
+
+
+const theme = {
+  colors: {
+    primary: "#c71111",
+    huseyin: "blue"
+  },
+  fontSizes: [8, 10, 12, 14, 16, 18, 20, 22],
+};
+
+
+ConfigProvider.config({
+  theme: {
+    primaryColor: theme.colors.primary,
+  }
+});
 
 function App( ) {
+  const [state, setState] = useState(1);
+
+ 
   return (
-      <Test>Test</Test>
+    <ThemeProvider theme={theme}>
+      {state}
+        <Button onClick={() => {
+          ConfigProvider.config({
+            theme: {
+              primaryColor: Math.random()> 0.5 ? theme.colors.primary : "blue",
+            },
+          });
+          setState(state+1)}}  type="primary">Primary Button</Button>
+              <Input size="large" placeholder="large size"/>
+              <Text fontSize={0} color="huseyin">Text</Text>
+          </ThemeProvider>
   );
 }
 
--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,10 @@
 import { Text } from "rebass";
+import { useTheme } from 'emotion-theming'
 
 function Test() {
+  console.log(useTheme())
   return (
-    <Text fontSize={6} color="primary" fontWeight="bold">
+    <Text fontSize={0} color="primary" fontWeight="bold">
       Text
     </Text>
   );
