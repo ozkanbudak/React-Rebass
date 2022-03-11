b3ace2ae68ec0f3345a34637417f57c4d5f10be0
Ozkan Hasan BUDAK <ozkanhasanbudak@192.168.1.62>
2022-03-11 23:12:20 +0300

    package

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,51 @@
-import { ThemeProvider } from 'emotion-theming';
-import Test from './Test';
-import preset from '@rebass/preset'
+import { ThemeProvider } from "emotion-theming";
+import Test from "./Test";
 import _ from "lodash";
 import { Text } from "rebass";
-import { ConfigProvider, Button, Input } from 'antd';
-import { Typography } from 'antd';
-import { useState } from "react"
-import 'antd/dist/antd.variable.min.css';
-import './ant.less';
-
+import { ConfigProvider, Button, Input } from "antd";
+import { Typography } from "antd";
+import { useState } from "react";
+import "antd/dist/antd.variable.min.css";
+import "./ant.less";
 
 const theme = {
   colors: {
     primary: "#c71111",
-    huseyin: "blue"
+    huseyin: "blue",
   },
   fontSizes: [8, 10, 12, 14, 16, 18, 20, 22],
 };
 
-
 ConfigProvider.config({
   theme: {
     primaryColor: theme.colors.primary,
-  }
+  },
 });
 
-function App( ) {
+function App() {
   const [state, setState] = useState(1);
 
- 
   return (
     <ThemeProvider theme={theme}>
       {state}
-        <Button onClick={() => {
+      <Button
+        onClick={() => {
           ConfigProvider.config({
             theme: {
-              primaryColor: Math.random()> 0.5 ? theme.colors.primary : "blue",
+              primaryColor: Math.random() > 0.5 ? theme.colors.primary : "blue",
             },
           });
-          setState(state+1)}}  type="primary">Primary Button</Button>
-              <Input size="large" placeholder="large size"/>
-              <Text fontSize={0} color="huseyin">Text</Text>
-          </ThemeProvider>
+          setState(state + 1);
+        }}
+        type="primary"
+      >
+        Primary Button
+      </Button>
+      <Input placeholder="large size" />
+      <Text fontSize={0} color="huseyin">
+        Text
+      </Text>
+    </ThemeProvider>
   );
 }
 
